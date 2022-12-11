2a789cc1b9af4e3eaf6084bb4b6d8560fce69a48
Drew Olsen <olsenj.andrew@gmail.com>
2022-12-11 02:01:26 -0600

    Config adjustments

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    stripeKey:
-      'sk_test_51MAewqAimYsCeNwXoVLP63zvnfu8Qatj2CgdeJlxSPmZfjqaMDRd9pn0RzO5psArSLiz7w3ENfukLujcoK6wxoIx00MeXSACjI',
-    stripeEndpoint: 'https://stripe.com/docs/api/tokens/create_card',
-  },
 };
 
 module.exports = nextConfig;
